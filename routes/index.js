a72f6c38605c4baf7f4d2b91b73ca35e5e041613
AtalaMellina <mellynatrindade@gmail.com>
2023-09-29 16:04:06 -0300

    Teste

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.query)
+  res.render('index', req.query);
+});
+
+router.get('/:title/:projeto', function(req, res, next) {
+  console.log(req.query)
+  res.render('index', req.query);
+});
+module.exports = router;
